3c2882c7c872df434d55d334a6154d1b9b3cc3c3
Daehwan Nam <dhnam@postech.ac.kr>
2019-11-01 20:56:52 +0900

    example.py is modified

--- example.py
+++ example.py
@@ -2,7 +2,6 @@
 # modified example from https://pytorch.org/docs/1.2.0/nn.html#lstm
 
 import torch
-import torch.nn as nn
 import rnn_util
 
 
@@ -11,7 +10,7 @@ num_directions = 2 if bidirectional else 1
 
 rnn = rnn_util.LayerNormLSTM(10, 20, 2, dropout=0.3, r_dropout=0.25,
                              bidirectional=bidirectional, layer_norm_enabled=True)
-# rnn = nn.LSTM(10, 20, 2, bidirectional=bidirectional)
+# rnn = torch.nn.LSTM(10, 20, 2, bidirectional=bidirectional)
 
 input = torch.randn(5, 3, 10)
 h0 = torch.randn(2 * num_directions, 3, 20)
