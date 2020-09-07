08f77a958e709de66077a4a534d3ceb8a0ccef5d
Daehwan Nam <dhnam@postech.ac.kr>
2020-09-07 10:15:24 +0900

    example.py is updated

--- example.py
+++ example.py
@@ -2,19 +2,52 @@
 # modified example from https://pytorch.org/docs/1.2.0/nn.html#lstm
 
 import torch
+from torch.nn import LSTMCell
 import rnn_util
 
+seq_len = 5
+batch_size = 3
+input_size = 10
 
+hidden_size = 20
+num_layers = 2
+dropout = 0.3
+r_dropout = 0.25
 bidirectional = True
 num_directions = 2 if bidirectional else 1
 
-rnn = rnn_util.LayerNormLSTM(10, 20, 2, dropout=0.3, r_dropout=0.25,
-                             bidirectional=bidirectional, layer_norm_enabled=True)
-# rnn = torch.nn.LSTM(10, 20, 2, bidirectional=bidirectional)
+# ------------------------ Examples of LSTMFrame ------------------------
+rnn_cells = [
+    [LSTMCell(input_size, hidden_size),
+     LSTMCell(input_size, hidden_size)],  # 1st bidirectional LSTM layer
+    [LSTMCell(hidden_size * num_directions, hidden_size),
+     LSTMCell(hidden_size * num_directions, hidden_size)]  # 2nd bidirectional LSTM layer
+]
+# 'rnn_cells' is a list of forward/backward rnn cell pairs.
+# Each pair corresponds to a layer of bidirectional LSTM.
 
-input = torch.randn(5, 3, 10)
-h0 = torch.randn(2 * num_directions, 3, 20)
-c0 = torch.randn(2 * num_directions, 3, 20)
+assert len(rnn_cells) == num_layers
+assert all(len(rnn_layer_cells) == num_directions for rnn_layer_cells in rnn_cells)
+
+rnn = rnn_util.LSTMFrame(rnn_cells, dropout=dropout, bidirectional=bidirectional)
+# rnn = torch.nn.LSTM(input_size, hidden_size, num_layers, bidirectional=bidirectional)
+
+input = torch.randn(seq_len, batch_size, input_size)
+h0 = torch.randn(num_layers * num_directions, batch_size, hidden_size)
+c0 = torch.randn(num_layers * num_directions, batch_size, hidden_size)
+output, (hn, cn) = rnn(input, (h0, c0))
+
+print(output.size())
+
+
+# ------------------------ Examples of LayerNormLSTM ------------------------
+rnn = rnn_util.LayerNormLSTM(input_size, hidden_size, num_layers, dropout=dropout, r_dropout=r_dropout,
+                             bidirectional=bidirectional, layer_norm_enabled=True)
+# rnn = torch.nn.LSTM(input_size, hidden_size, num_layers, bidirectional=bidirectional)
+
+input = torch.randn(seq_len, batch_size, input_size)
+h0 = torch.randn(num_layers * num_directions, batch_size, hidden_size)
+c0 = torch.randn(num_layers * num_directions, batch_size, hidden_size)
 output, (hn, cn) = rnn(input, (h0, c0))
 
 print(output.size())
